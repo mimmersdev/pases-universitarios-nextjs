0192fe018cb715df04a9f7c2212e3361dcbbab2a
Mrjuanblack <dev@mimmers.com>
2026-01-22 15:46:36 -0500

    Updated installation page

--- app/install/[universityId]/[uniqueIdentifier]/[careerCode]/page.tsx
+++ app/install/[universityId]/[uniqueIdentifier]/[careerCode]/page.tsx
@@ -36,25 +36,25 @@ export default function InstallPage() {
     };
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen w-screen bg-[url('/install_bg.png')] bg-cover bg-center">
-            <img src="/logo.svg" alt="logo" className="w-100 h-auto" />
-            <h2 className="mt-10 text-6xl font-space-grotesk text-brand-main">Elige tu Ruta</h2>
-            <p className="mt-16 max-w-[700px] text-center text-2xl font-space-grotesk text-white">
+        <div className="p-2 flex flex-col items-center justify-center h-screen w-screen bg-[url('/install_bg.png')] bg-cover bg-center">
+            <img src="/logo_white.svg" alt="logo" className="w-80 md:w-100 h-auto" />
+            <h2 className="mt-10 text-4xl md:text-6xl font-space-grotesk text-brand-main">Elige tu Ruta</h2>
+            <p className="mt-8 md:m-16 max-w-[700px] text-center text-lg md:text-2xl font-space-grotesk text-white">
                 Bienvenido a RutaPro. Puedes instalar tu pase universitario en Google Wallet o Apple Wallet.
             </p>
-            <p className="mt-4 text-center text-2xl font-space-grotesk text-white">
+            <p className="mt-2 md:mt-4 text-center text-lg md:text-2xl font-space-grotesk text-white">
                 No es necesario instalar aplicaciones adicionales.
             </p>
 
-            <p className="mt-16 text-center text-2xl font-arvo text-brand-main">
+            <p className="mt-8 md:mt-16 text-center text-lg md:text-2xl font-arvo text-brand-main">
                 Escoge la plataforma de tu preferencia:
             </p>
             <div className="mt-8 flex flex-col items-center justify-center gap-4">
-                <img src="/addtowallet/apple.svg" alt="apple wallet" className="w-64 h-auto cursor-pointer" onClick={handleDownloadApplePass} />
-                <img src="/addtowallet/google.svg" alt="google wallet" className="w-64 h-auto cursor-pointer" onClick={handleGetGoogleInstallLink} />
+                <img src="/addtowallet/apple.svg" alt="apple wallet" className="w-52 md:w-64 h-auto cursor-pointer" onClick={handleDownloadApplePass} />
+                <img src="/addtowallet/google.svg" alt="google wallet" className="w-52 md:w-64 h-auto cursor-pointer" onClick={handleGetGoogleInstallLink} />
             </div>
 
-            <p className="mt-16 text-center text-xl font-space-grotesk text-gray-500">
+            <p className="mt-8 md:mt-16 text-center text-lg md:text-xl font-space-grotesk text-gray-300">
                 Plataforma AVEX - Desarrollado por <a href="https://mimmers.com" target="_blank" className="text-brand-main">Mimmers</a>
             </p>
         </div>
